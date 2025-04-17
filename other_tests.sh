743c7e9c897a35692e75cc9b4750dafbe43271e4
Simo Toivanen <posti@simotoivanen.fi>
2025-04-17 12:21:17 +0300

    Muokattu...

--- other_tests.sh
+++ other_tests.sh
@@ -2,6 +2,11 @@
 
 cd Frontend
 
+echo "Compiling and testing the frontend"
+
+echo "Installing frontend dependencies"
+npm install
+
 echo "Checking code style"
 npm run lint
 
@@ -11,6 +16,11 @@ npm test
 cd ..
 
 cd Backend
+
+echo "Compiling and testing the backend"
+
+echo "Installing backend dependencies"
+npm install
 
 echo "Running tests"
 npm test
