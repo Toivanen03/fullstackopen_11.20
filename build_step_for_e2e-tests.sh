f3cd367f70e3ff347b567db1bfd941ddf8f3410e
Simo Toivanen <posti@simotoivanen.fi>
2025-04-17 09:17:23 +0300

    pm2-testi

--- build_step_for_e2e-tests.sh
+++ build_step_for_e2e-tests.sh
@@ -2,14 +2,15 @@
 
 echo "Installing Playwright dependencies"
 npm ci
+npm install --save-dev wait-on
 
 cd Backend
 
 echo "Installing backend dependencies"
 npm ci
 
-echo "Starting backend server"
-npm run start &
+echo "Starting backend server with pm2"
+npx pm2 start npm --name backend -- run start
 
 cd ..
 
@@ -21,7 +22,9 @@ npm ci
 echo "Compiling build"
 npm run build
 
-echo "Starting frontend server"
-npm run preview -- --port 4173 &
+echo "Starting frontend server with pm2"
+npx pm2 start npm --name frontend -- run preview -- --port 4173
 
 cd ..
+
+wait
